ae1a15a37591deaa368b11613e914be25a0e7dc0
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-08-26 12:48:58 +0300

    Update divizorixyz.c

--- divizorixyz.c
+++ divizorixyz.c
@@ -3,45 +3,20 @@
 int main()
 {
 	unsigned n, x, y, z;
-	unsigned short d, dif;
-	char found = 0;
 
 	scanf("%u", &n);
 
-	if (n < 12) printf("nu exista");
-	else {
-		d = 2;
-		while (d*d < n) {
-			if (n % d == 0) {
-				x = d;
-				y = x*2;
-				z = x*3;
-
-				dif = n - (x + y + z);
-
-				while (dif >= 0 && z <= n/2) {
-					if (dif == 0) {
-						found = 1;
-
-						printf("%u %u %u\n", x, y, z);
-
-						x = y;
-						y = x*2;
-						z = x*3;
-					} else {
-						z = x*(3 + dif/x);
-					}
-					
-					dif = n - (x + y +z);
-				}
-			}
-
-			d++;
-		}
-
-		if (d*d == n || !found) printf("nu exista");
+	if (n >= 12 && n % 2 == 0 && n/2 % 3 == 0) {
+		z = n/2;
+		x = z/3;
+		y = x*2;
+		
+		printf("%u %u %u", x, y, z);
+	} else {
+		printf("nu exista");
 	}
 
 	return 0;
 }
-// scor 20
+
+// scor 100
